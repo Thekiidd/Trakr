75328d2bd346c334a7bba5b8469711195168cb15
Luis Batista <lbatistabustillos@gmail.com>
2025-03-15 13:47:31 -0600

    Actualizacion Integradora7

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,14 +28,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "19364d8c2f741549da0a7746f886415c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "e575c265fd70eda4f6e088507eeae78d",
+"flutter_bootstrap.js": "f7fac87139cea38c5476ca54d32631b2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "b3497c18dc8cb12c4e16060d7eb697df",
 "/": "b3497c18dc8cb12c4e16060d7eb697df",
-"main.dart.js": "312592cc0f83992b6d80b908b9025101",
+"main.dart.js": "90cecf5d539c2a56c782faf5bd8e7f0d",
 "manifest.json": "c19276d56ec2c32ffd87a7b6c80efe57",
 "version.json": "bba1d6e3b437e6acc7aaeff1fdc299fd"};
 // The application shell files that are downloaded before a service worker can
--- lib/views/forum/forum_screen.dart
+++ lib/views/forum/forum_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 import '../../core/theme/app_theme.dart';
 import '../../models/post_model.dart';
