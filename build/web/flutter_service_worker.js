fb9dda11a575bf334f976e3f2fece2cbc2b4fda0
Luis Batista <lbatistabustillos@gmail.com>
2025-03-29 13:14:24 -0600

    Actualizacion02

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "19364d8c2f741549da0a7746f886415c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "2007d8857aec17a5dfd93887772c3e73",
+"flutter_bootstrap.js": "6b77303868a5435e3adc234a67d3f9d7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
